994f48b6c10fdd132ccee929ec1ee4b8d94b5717
cplepel-py <cbplepel@gmail.com>
2020-10-08 21:38:15 -0500

    Added basic node class

---
+++ src/main/java/frc/robot/path/dstar/Node.java
@@ -0,0 +1,25 @@
+package frc.robot.path.dstar;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Node {
+  public List<Node> edges;
+	double x;
+	double y;
+	public double g;
+	public double rhs;
+  
+  public Node(double x, double y, List<Node> edges) {
+		this.x = x;
+		this.y = y;
+		this.edges = edges;
+	}
+	public Node(double x, double y) {
+		this(x, y, new ArrayList<Node>());
+	}
+	
+	public double weightTo(Node dest) {
+		return Math.sqrt(Math.pow(x - dest.x, 2) + Math.pow(y - dest.y, 2));
+	}
+}

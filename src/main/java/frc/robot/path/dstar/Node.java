9bf2f3f2b06f706594952d29ecb22f8e3ba25171
cplepel-py <cbplepel@gmail.com>
2020-10-10 13:38:39 -0500

    Added key method to Node class

--- src/main/java/frc/robot/path/dstar/Node.java
+++ src/main/java/frc/robot/path/dstar/Node.java
@@ -5,21 +5,25 @@ import java.util.ArrayList;
 
 public class Node {
   public List<Node> edges;
-	double x;
-	double y;
-	public double g;
-	public double rhs;
+  double x;
+  double y;
+  public double g;
+  public double rhs;
   
   public Node(double x, double y, List<Node> edges) {
-		this.x = x;
-		this.y = y;
-		this.edges = edges;
-	}
-	public Node(double x, double y) {
-		this(x, y, new ArrayList<Node>());
-	}
-	
-	public double weightTo(Node dest) {
-		return Math.sqrt(Math.pow(x - dest.x, 2) + Math.pow(y - dest.y, 2));
-	}
+    this.x = x;
+    this.y = y;
+    this.edges = edges;
+  }
+  public Node(double x, double y) {
+    this(x, y, new ArrayList<Node>());
+  }
+  
+  public double weightTo(Node dest) {
+    return Math.sqrt(Math.pow(x - dest.x, 2) + Math.pow(y - dest.y, 2));
+  }
+
+  public double key() {
+    return Math.min(g, rhs);
+  }
 }

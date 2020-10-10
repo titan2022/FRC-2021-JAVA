2cd824bfe7882e0c20ffe4fc35baa556d3318b5d
cplepel-py <cbplepel@gmail.com>
2020-10-10 14:07:53 -0500

    Added greedy method to get next node in path

--- src/main/java/frc/robot/path/dstar/Node.java
+++ src/main/java/frc/robot/path/dstar/Node.java
@@ -2,6 +2,8 @@ package frc.robot.path.dstar;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Collections;
 
 public class Node implements Comparable<Node> {
   public List<Node> edges;
@@ -23,6 +25,10 @@ public class Node implements Comparable<Node> {
     return Math.sqrt(Math.pow(x - dest.x, 2) + Math.pow(y - dest.y, 2));
   }
 
+  public Node getNext() {
+    return Collections.min(edges, new Node.GoalDistOrder(this));
+  }
+
   public double key() {
     return Math.min(g, rhs);
   }
@@ -30,4 +36,16 @@ public class Node implements Comparable<Node> {
   public int compareTo(Node o) {
     return (int) Math.signum(o.key() - key());
   }
+
+  private class GoalDistOrder implements Comparator<Node> {
+		Node from;
+		GoalDistOrder(Node from) {
+			this.from = from;
+		}
+		
+		@Override
+		public int compare(Node o1, Node o2) {
+			return (int) Math.signum(from.weightTo(o1) + o1.g - from.weightTo(o2) - o2.g);
+		}
+	}
 }

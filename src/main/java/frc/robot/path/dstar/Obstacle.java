821be44ebd0696bc27c3efbbf722ac2b968c537e
cplepel-py <cbplepel@gmail.com>
2020-10-09 21:41:01 -0500

    Added method to test if path intersects an obstacle

--- src/main/java/frc/robot/path/dstar/Obstacle.java
+++ src/main/java/frc/robot/path/dstar/Obstacle.java
@@ -3,9 +3,26 @@ package frc.robot.path.dstar;
 import java.util.List;
 
 public class Obstacle {
-	public final List<Node> vertexes;
-	
-	public Obstacle(List<Node> vertexes) {
-		this.vertexes = List.copyOf(vertexes);
+  public final List<Node> vertexes;
+  
+  public Obstacle(List<Node> vertexes) {
+    this.vertexes = List.copyOf(vertexes);
+  }
+  
+  private static double getAngle(Node base, Node vertex, Node leg) {
+    return (Math.atan2(leg.y - vertex.y, leg.x - vertex.x) -
+      Math.atan2(base.y - vertex.y, base.x - vertex.x)) % 180.;
+  }
+  
+  public boolean isClear(Node a, Node b) {
+    Node x, y;
+    for(int i=0; i<vertexes.size(); i++){
+      x = vertexes.get(i);
+      y = vertexes.get((i+1) % vertexes.size());
+      if(getAngle(a, b, x) * getAngle(a, b, y) < 0 &&
+         getAngle(x, y, a) * getAngle(x, y, b) < 0)
+        return false;
     }
+    return true;
+  }
 }

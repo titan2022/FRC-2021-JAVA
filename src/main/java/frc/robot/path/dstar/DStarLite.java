da494528fd9871150aa6de2b656cfcc85d5e9cce
cplepel-py <cbplepel@gmail.com>
2020-10-11 18:18:14 -0500

    Added Javadoc for public DStarLite methods

--- src/main/java/frc/robot/path/dstar/DStarLite.java
+++ src/main/java/frc/robot/path/dstar/DStarLite.java
@@ -27,6 +27,15 @@ public class DStarLite {
     queue.add(goal);
   }
 
+  /**
+   * Computes the shortest to the goal and returns the next node along the path.
+   * 
+   * This method also automatically creates edges to connect nodes to their
+   * relevant neighbors as necessary, so connections between nodes do not have
+   * to be pre-specified.
+   * 
+   * @return  The next node along the shortest path to the goal node.
+   */
   public Node getPath() {
     while(queue.size() > 0 && (queue.peek().key() < start.key() || start.g != start.rhs)){
       Node v = queue.poll();
@@ -70,6 +79,15 @@ public class DStarLite {
     v.visited = true;
   }
 
+  /**
+   * Determines if a straight path between two nodes is free of obstacles.
+   * 
+   * @param a  The node at one endpoint of the straight path.
+   * @param b  The node at the other endpoint of the straight path.
+   * @return  Either true, if the path does not pass through the interior of any
+   *  obstacles, or false, otherwise.
+   * @see Obstacle#isClear(Node, Node)
+   */
   public boolean isClear(Node a, Node b) {
     for(Obstacle obs : map)
       if(!obs.isClear(a, b))
@@ -77,6 +95,17 @@ public class DStarLite {
     return true;
   }
 
+  /**
+   * Adds an obstacle to the internal map used to compute the shortest path.
+   * 
+   * This method automatically connects each vertex of the new obstacle to all
+   * relevant neighbors.
+   * 
+   * This can be used along with {@link #deleteObstacle(Obstacle)} to change an
+   * obstacle.
+   * 
+   * @param obstacle  The obstacle to add to the map.
+   */
   public void addObstacle(Obstacle obstacle) {
     map.add(obstacle);
     for(Node node : obstacle.vertexes){
@@ -84,6 +113,17 @@ public class DStarLite {
     }
   }
 
+  /**
+   * Removes an obstacle from the internal map used to compute the shortest path.
+   * 
+   * This method automatically informs the neighbors of each of the vertexes of
+   * the removed obstacle and severs the relevant edges.
+   * 
+   * This can be used along with {@link #addObstacle(Obstacle)} to change an
+   * obstacle.
+   * 
+   * @param obstacle  The obstacle to remove from the map.
+   */
   public void deleteObstacle(Obstacle obstacle) {
     map.remove(obstacle);
     for(Node vertex : obstacle.vertexes){

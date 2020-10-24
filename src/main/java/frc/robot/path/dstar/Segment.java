1093c4d7caeb53b65d5e2b3aa58b64bcfb71a8b9
cplepel-py <cbplepel@gmail.com>
2020-10-23 23:31:06 -0500

    Added default implementations to Segment methods

--- src/main/java/frc/robot/path/dstar/Segment.java
+++ src/main/java/frc/robot/path/dstar/Segment.java
@@ -11,18 +11,32 @@ public interface Segment {
 
   // differential drive
   public Rotation2d getRotation(double distance);
-  public Rotation2d angularVelocity(double distance, double speed);
+  public default Rotation2d angularVelocity(double distance, double speed) {
+    return angularVelocity(distance).times(speed);
+  }
   public Rotation2d angularVelocity(double distance);
 
   // holonomic drive
-  public Translation2d getVelocity(double distance, double speed);
-  public Translation2d getVelocity(double distance);
-  public Translation2d getAcceleration(double distance, double speed, double accel);
-  public Translation2d getAcceleration(double distance, double speed);
-  public Translation2d getAcceleration(double distance);
+  public default Translation2d getVelocity(double distance, double speed) {
+    return getVelocity(distance).times(speed);
+  }
+  public default Translation2d getVelocity(double distance) {
+    return new Translation2d(1, 0).rotateBy(getRotation(distance));
+  }
+  public default Translation2d getAcceleration(double distance, double speed, double accel) {
+    return getAcceleration(distance, speed)
+      .plus(new Translation2d(accel, 0).rotateBy(getRotation(distance)));
+  }
+  public default Translation2d getAcceleration(double distance, double speed) {
+    return getAcceleration(distance).times(speed);
+  }
+  public default Translation2d getAcceleration(double distance) {
+    return new Translation2d(angularVelocity(distance).getRadians(), 0)
+      .rotateBy(getRotation(distance));
+  }
 
   // transformations
   public Segment translate(Translation2d offset);
   public Segment rotate(Rotation2d rotation);
-  public Segment rotate(Transform2d transformation);
+  public Segment transform(Transform2d transformation);
 }

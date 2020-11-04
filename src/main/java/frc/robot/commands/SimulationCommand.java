20695423c802255538dfbffe6459fbca1bc5fabd
Jaden <jwang5@imsa.edu>
2020-11-03 22:43:47 -0600

    Adjusted SimulationCommand and Robot.java

--- src/main/java/frc/robot/commands/SimulationCommand.java
+++ src/main/java/frc/robot/commands/SimulationCommand.java
@@ -21,7 +21,7 @@ import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-//import frc.wpilibjTemp.Field2d;
+import frc.wpilibjTemp.Field2d;
 
 /**
  * An example command that uses an example subsystem, like a boss.
@@ -35,6 +35,7 @@ public class SimulationCommand extends CommandBase {
   private Field2d f;
   private Timer timer;
   private double previousTime;
+  
 
   /**
    * Creates a new ExampleCommand.
@@ -53,15 +54,13 @@ public class SimulationCommand extends CommandBase {
     //set the speeds of each wheel and stuff
     //set up the simulation environment w position
 
-    
-    f.setRobotPose(0, 0, 0);
-    vl = 5;
-    vr = 10;
-
     timer = new Timer();
     f = new Field2d();
     timer.start();
     previousTime = timer.get();
+    f.setRobotPose(0, 0, new Rotation2d(0));
+    vl = 5;
+    vr = 10;
 
 
     //DifferentialJacobianBuilder = new MatBuilder<>(Nat.N3(), Nat.N2());
@@ -73,8 +72,9 @@ public class SimulationCommand extends CommandBase {
   public void execute() {
     //import my kinematics class and run the kinematics command on the previous position and the given left and right wheel velocities
     DifferentialDriveKinematics object = new DifferentialDriveKinematics(1, 1);
-    DifferentialJacobian = object.getAbsoluteVelocity(vl, vr, f.getRobotPose().getRotation());
-    f.setRobotPose(DifferentialJacobian.get(1,1), DifferentialJacobian.get(2,1), DifferentialJacobian.get(3,1));
+    DifferentialJacobian = object.getAbsoluteVelocity(vl, vr, f.getRobotPose().getRotation().getRadians());
+    f.setRobotPose(DifferentialJacobian.get(1,1), DifferentialJacobian.get(2,1), new Rotation2d(DifferentialJacobian.get(3,1)));
+    previousTime = timer.get();
   }
 
   // Called once the command ends or is interrupted.

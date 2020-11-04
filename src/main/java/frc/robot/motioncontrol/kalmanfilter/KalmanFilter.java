e876679ddcff224ce8728e9222d0cb622ab9c4c7
rajpatkar <rajan.patkar.email@gmail.com>
2020-11-03 22:35:32 -0600

    More class variables and constructor

--- src/main/java/frc/robot/motioncontrol/kalmanfilter/KalmanFilter.java
+++ src/main/java/frc/robot/motioncontrol/kalmanfilter/KalmanFilter.java
@@ -2,15 +2,30 @@ package frc.robot.motioncontrol.kalmanfilter;
 
 public class KalmanFilter {
 
-    // class variables, x is state vector not position
+    // class variables
 
     private double tDelta;
-    private double[][] x;
+    private double a; // given acceleration
     private double aNoise;
-    private double a;
-    private double[][] transitionMatrix;
+    private double measNoise; // measurement noise
+    private double sz; // measurement noise covariance
+    private double[][] x; // state vector
+    private double[][] transMatrix; // transition matrix
     private double[][] inputMatrix;
-    private double[][] measMatrix;
-    private double measNoise;
-    
+    private double[][] measMatrix; // measurement matrix
+    private double[][] sw; // process noise covariance
+
+    public KalmanFilter(double tDelta, double a, double aNoise, double measNoise) {
+
+        this.tDelta = tDelta;
+        this.a = a;
+        this.aNoise = aNoise;
+        this.measNoise = measNoise;
+        transMatrix = new double[][] { { 1, tDelta }, { 0, 1 } };
+        inputMatrix = new double[][] { { Math.pow(tDelta, 2) / 2 }, { tDelta } };
+        measMatrix = new double[][] { { 1, 0 } };
+        x = new double[][] { { 0 }, { 0 } };
+
+    }
+
 }

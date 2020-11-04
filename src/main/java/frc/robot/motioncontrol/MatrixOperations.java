699c41c28e35797f460eefe25bcf69e6748f3646
rajpatkar <rajan.patkar.email@gmail.com>
2020-11-04 14:08:34 -0600

    Added matrix multiplication method

--- src/main/java/frc/robot/motioncontrol/MatrixOperations.java
+++ src/main/java/frc/robot/motioncontrol/MatrixOperations.java
@@ -54,4 +54,34 @@ public class MatrixOperations {
 
     }
 
+    /**
+     * Multiplies 2 arrays (order matters and array1.length == array2[i].length)
+     * 
+     * @param array1 - First 2D double array.
+     * @param array2 - Second 2D double array.
+     * @return Product of two matrices.
+     */
+    
+    public static double[][] matrixMultiply(double[][] array1, double[][] array2) {
+
+        double[][] productMatrix = new double[array1.length][array2[0].length];
+
+        for (int i = 0; i < array1.length; i++) {
+
+            for (int j = 0; j < array2[0].length; j++) {
+
+                for (int k = 0; k < array1[0].length; k++) {
+
+                    productMatrix[i][j] += array1[i][k] * array2[k][j];
+
+                }
+
+            }
+
+        }
+
+        return productMatrix;
+
+    }
+
 }

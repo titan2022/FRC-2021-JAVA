d4f5fdd4005a756a436cea4bbafd04ea196ee1a6
rajpatkar <rajan.patkar.email@gmail.com>
2020-11-03 21:25:33 -0600

    Added class variables

--- src/main/java/frc/robot/motioncontrol/kalmanfilter/KalmanFilter.java
+++ src/main/java/frc/robot/motioncontrol/kalmanfilter/KalmanFilter.java
@@ -1,5 +1,16 @@
 package frc.robot.motioncontrol.kalmanfilter;
 
 public class KalmanFilter {
+
+    // class variables, x is state vector not position
+
+    private double tDelta;
+    private double[][] x;
+    private double aNoise;
+    private double a;
+    private double[][] transitionMatrix;
+    private double[][] inputMatrix;
+    private double[][] measMatrix;
+    private double measNoise;
     
 }

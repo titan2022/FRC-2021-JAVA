139bbc38dd807796502b1fddb070c16a30fe2696
rajpatkar <rajan.patkar.email@gmail.com>
2020-11-04 08:12:12 -0600

    Added scalarMultiply method

--- src/main/java/frc/robot/motioncontrol/kalmanfilter/KalmanFilter.java
+++ src/main/java/frc/robot/motioncontrol/kalmanfilter/KalmanFilter.java
@@ -27,6 +27,29 @@ public class KalmanFilter {
         measMatrix = new double[][] { { 1, 0 } };
         x = new double[][] { { 0 }, { 0 } };
         sz = Math.pow(measNoise, 2);
+        sw = new double[][] {};
+
+    }
+
+    /**
+     * Multiplies 2D array by a scalar
+     * 
+     * @param scalar - Scalar to multiply by.
+     * @param array  - Array to be multiplied (class variable).
+     * 
+     */
+
+    private static void scalarMultiply(double scalar, double[][] array) {
+
+        for (int i = 0; i < array.length; i++) {
+
+            for (int j = 0; j < array[i].length; j++) {
+
+                array[i][j] *= scalar;
+
+            }
+
+        }
 
     }
 

8d2e4c04b6e2f55e4ed4fb0acdef5d1e2acd669d
blahfacepants <adas@imsa.edu>
2020-11-11 16:24:54 -0600

    fixed getWheelVelocity

--- src/main/java/frc/robot/kinematics/DifferentialDriveKinematics.java
+++ src/main/java/frc/robot/kinematics/DifferentialDriveKinematics.java
@@ -32,9 +32,9 @@ public class DifferentialDriveKinematics {
 
     /**
      * 
-     * @param vL
-     * @param vR
-     * @param phi
+     * @param vL - the rotational velocity of the left wheel
+     * @param vR - the rotational velocity of the right wheel
+     * @param phi - the absolute rotational pose of the robot chassis in radians
      * @return
      */
     public Matrix getAbsoluteVelocity(double vL, double vR, double phi){
@@ -47,14 +47,15 @@ public class DifferentialDriveKinematics {
 
     /**
      * 
-     * @param vX
-     * @param vY
-     * @param phi
+     * @param vX - the x component of the chassis velocity relative to the field
+     * @param vY - the y component of the chassis velocity relative to the field
+     * @param dPhi - rotataional velocity in radians per second
+     * @param phi - the absolute rotational pose of the robot chassis in radians
      * @return
      */
-    public Matrix getWheelVelocity(double vX, double vY, double phi)
+    public Matrix getWheelVelocity(double vX, double vY, double dPhi, double phi)
     {
-        P = PBuilder.fill(vX, vY, phi);
+        P = PBuilder.fill(vX, vY, dPhi);
         DifferentialJacobian = InverseDifferentialJacobianBuilder.fill(Math.cos(phi), Math.sin(phi), a,
                                                                         Math.cos(phi), Math.sin(phi), -a);
         DifferentialJacobian = DifferentialJacobian.times(1.0/wR);

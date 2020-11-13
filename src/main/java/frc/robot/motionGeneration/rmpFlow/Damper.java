bf0048b1766beccdeb4bb7ec0e8a08870d619e35
SiddharthRT <40873352+SiddharthRT@users.noreply.github.com>
2020-11-13 17:41:30 -0500

    added Damper class

---
+++ src/main/java/frc/robot/motionGeneration/rmpFlow/Damper.java
@@ -0,0 +1,41 @@
+package frc.robot.motionGeneration.rmpFlow;
+
+import org.ejml.simple.SimpleMatrix;
+
+public class Damper extends RMPLeaf{
+    private double w;
+    private double eta;
+
+    public Damper(String name, RMPNode parent, double eta, double w){
+        super(name, parent);
+        this.eta = eta;
+        this.w = w;
+    }
+         
+    public SimpleMatrix phi(SimpleMatrix q)
+	{
+		return q;
+	}
+
+	public SimpleMatrix j(SimpleMatrix q)
+	{
+		return eye(2);
+	};
+
+	public SimpleMatrix j_dot(SimpleMatrix q, SimpleMatrix q_dot)
+	{
+		return new SimpleMatrix(2, 2);
+	};
+
+
+    public SimpleMatrix solveF(){
+        SimpleMatrix bx_dot = x_dot.scale(-(eta * w));
+        return bx_dot;
+    }
+
+    
+    public SimpleMatrix solveM(){
+        return new SimpleMatrix(1, 1, false, new double[]{w});
+    }
+
+}

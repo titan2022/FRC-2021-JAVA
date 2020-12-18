2e6e4098014293753bb8948939182b79a83b209e
rajpatkar <rajan.patkar.email@gmail.com>
2020-12-18 11:04:27 -0600

    started execute command with SmartDashboard

--- src/main/java/frc/robot/motioncontrol/kalmanfilter/KalmanFilterDemoCommand.java
+++ src/main/java/frc/robot/motioncontrol/kalmanfilter/KalmanFilterDemoCommand.java
@@ -1,7 +1,10 @@
 package frc.robot.motioncontrol.kalmanfilter;
 
 import org.ejml.simple.SimpleMatrix;
+import java.util.Random;
 
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.wpilibjTemp.Field2d;
 
@@ -9,6 +12,10 @@ public class KalmanFilterDemoCommand extends CommandBase {
 
     private Field2d field;
     private KalmanFilter filter;
+    private SimpleMatrix posReal;
+    private SimpleMatrix posHat;
+    private Rotation2d angle;
+    private Random rand;
 
     public KalmanFilterDemoCommand() {
 
@@ -20,12 +27,24 @@ public class KalmanFilterDemoCommand extends CommandBase {
     public void initialize() {
 
         field = new Field2d();
-        SimpleMatrix x = new SimpleMatrix(new double[][] { { 0 }, { 0 } });
+        angle = new Rotation2d();
+        posReal = new SimpleMatrix(new double[][] { { 0 }, { 0 } });
+        posHat = new SimpleMatrix(posReal);
         SimpleMatrix Q = SimpleMatrix.identity(2).scale(0.00001);
         SimpleMatrix R = SimpleMatrix.identity(2).scale(0.001);
 
-        filter = new KalmanFilter(x, Q, SimpleMatrix.identity(2), R, SimpleMatrix.identity(2), SimpleMatrix.identity(2),
+        filter = new KalmanFilter(posHat, Q, SimpleMatrix.identity(2), R, SimpleMatrix.identity(2), SimpleMatrix.identity(2),
                 SimpleMatrix.identity(2));
+
+    }
+
+    @Override
+    public void execute() {
+
+        field.setRobotPose(posReal.get(0, 0), posReal.get(1, 0), angle);
+        SmartDashboard.putNumber("x (real)", field.getRobotPose().getTranslation().getX());
+        SmartDashboard.putNumber("y (real)", field.getRobotPose().getTranslation().getY());
+
 
     }
 

74295f3e2f32a461223c49a237c18343e1cdad06
cplepel-py <cbplepel@gmail.com>
2020-12-27 18:17:49 -0600

    Added methods to get nodes and edges of DStarGraph

--- src/main/java/frc/robot/path/dstar/DStarGraph.java
+++ src/main/java/frc/robot/path/dstar/DStarGraph.java
@@ -1,14 +1,18 @@
 package frc.robot.path.dstar;
 
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
 import java.util.Map;
 import java.util.NavigableMap;
 import java.util.PriorityQueue;
 import java.util.Queue;
+import java.util.Set;
 
 import edu.wpi.first.wpilibj.geometry.Translation2d;
 import frc.robot.mapping.Obstacle;
 import frc.robot.mapping.ObstacleMap;
+import frc.robot.mapping.Path;
 import frc.robot.mapping.Point;
 
 public class DStarGraph {
@@ -33,5 +37,36 @@ public class DStarGraph {
     }
     public DStarGraph(ObstacleMap map, Translation2d goal) {
         this(map, goal, 0);
+    }
+
+    public Iterable<DStarNode> getNodes() {
+        Set<DStarNode> res = new LinkedHashSet<>();
+        res.add(goal);
+        res.add(start);
+        for(Map<Point, DStarNode> map : obstacleSets.values())
+            res.addAll(map.values());
+        return res;
+    }
+
+    public Iterable<Path> getEdges() {
+        return new Iterable<Path>() {
+            @Override
+            public Iterator<Path> iterator() {
+                return new Iterator<Path>() {
+                    Iterator<DStarNode> nodeIter = getNodes().iterator();
+                    Iterator<Path> edgeIter = null;
+                    @Override
+                    public boolean hasNext() {
+                        return nodeIter.hasNext() || (edgeIter != null && edgeIter.hasNext());
+                    }
+                    @Override
+                    public Path next() {
+                        while(edgeIter == null || !edgeIter.hasNext())
+                            edgeIter = nodeIter.next().getEdges().iterator();
+                        return edgeIter.next();
+                    }
+                };
+            }
+        };
     }
 }

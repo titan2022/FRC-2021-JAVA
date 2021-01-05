f1664d855e4e3ddd777c0af0e23b4a8e7ee499ce
cplepel-py <cbplepel@gmail.com>
2021-01-04 22:42:23 -0600

    Added tests of update and rectify methods

--- src/main/java/frc/robot/path/dstar/DStarTester.java
+++ src/main/java/frc/robot/path/dstar/DStarTester.java
@@ -12,6 +12,8 @@ public class DStarTester extends CommandBase {
     public void initialize() {
         System.out.print("Node methods: ");
         System.out.println(testNode());
+        System.out.print("D* Lite primitives: ");
+        System.out.println(testPrimitives());
     }
 
     @Override
@@ -77,6 +79,57 @@ public class DStarTester extends CommandBase {
         truth = b.getDegree() == 0;
         allPass &= truth;
         truth = a.getRhs() == c.getG() + 2;
+        allPass &= truth;
+        return allPass;
+    }
+
+    public boolean testPrimitives() {
+        boolean truth = true;
+        boolean allPass = true;
+        Queue<DStarNode> queue = new PriorityQueue<>();
+        DStarNode a = new DStarNode(new Point(1, 1), queue);
+        a.update();
+        truth = queue.size() == 0;
+        allPass &= truth;
+        a.rectify();
+        truth = queue.size() == 0;
+        allPass &= truth;
+        truth = a.getG() == Double.POSITIVE_INFINITY;
+        allPass &= truth;
+        truth = a.getRhs() == Double.POSITIVE_INFINITY;
+        allPass &= truth;
+        DStarNode b = new DStarNode(new Point(0, 1), queue, 0, 1);
+        b.update();
+        truth = queue.size() == 1;
+        allPass &= truth;
+        b.rectify();
+        truth = queue.size() == 0;
+        allPass &= truth;
+        truth = b.getG() == Double.POSITIVE_INFINITY;
+        allPass &= truth;
+        truth = b.getRhs() == 1;
+        allPass &= truth;
+        DStarNode c = new DStarNode(new Point(1, 0), queue, 1, 0);
+        c.update();
+        truth = queue.size() == 1;
+        allPass &= truth;
+        c.rectify();
+        truth = queue.size() == 0;
+        allPass &= truth;
+        truth = c.getG() == 0;
+        allPass &= truth;
+        truth = c.getRhs() == 0;
+        allPass &= truth;
+        DStarNode d = new DStarNode(new Point(0, 0), queue, 0, 0);
+        d.update();
+        truth = queue.size() == 0;
+        allPass &= truth;
+        d.rectify();
+        truth = queue.size() == 0;
+        allPass &= truth;
+        truth = d.getG() == 0;
+        allPass &= truth;
+        truth = d.getRhs() == 0;
         allPass &= truth;
         return allPass;
     }

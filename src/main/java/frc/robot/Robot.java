737ebe5ec2e657bd3952c5f07766be385dcbba69
cplepel-py <cbplepel@gmail.com>
2021-01-04 22:15:42 -0600

    Added DStarTester to autonomous init for testing

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -10,6 +10,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.path.dstar.DStarTester;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -71,6 +72,7 @@ public class Robot extends TimedRobot {
     if (m_autonomousCommand != null) {
       m_autonomousCommand.schedule();
     }
+    new DStarTester().schedule();
   }
 
   /**

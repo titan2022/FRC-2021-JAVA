f1b446df14049f43c0ac203cb939e006561625d3
cplepel-py <cbplepel@gmail.com>
2021-01-08 16:35:36 -0600

    Scheduled DStarDemoCommand to be run during autonomous

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -10,6 +10,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.path.dstar.DStarDemoCommand;
 import frc.robot.path.dstar.DStarTester;
 
 /**
@@ -73,6 +74,7 @@ public class Robot extends TimedRobot {
       m_autonomousCommand.schedule();
     }
     new DStarTester().schedule();
+    new DStarDemoCommand().schedule();
   }
 
   /**

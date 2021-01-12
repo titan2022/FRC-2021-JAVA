05b75187325a00fcaa087c5cc4944c46d9b75745
rajpatkar <rajan.patkar.email@gmail.com>
2021-01-12 14:13:22 -0600

    Added Drive and Navigation subsystem files

---
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -0,0 +1,6 @@
+package frc.robot.subsystems;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class DriveSubsystem extends SubsystemBase {
+
+}
---
+++ src/main/java/frc/robot/subsystems/NavigationSubsystem.java
@@ -0,0 +1,6 @@
+package frc.robot.subsystems;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class NavigationSubsystem extends SubsystemBase {
+
+}

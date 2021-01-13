b231d8ab72794ef0f765a96ac26f0835ff8c9ed9
rajpatkar <rajan.patkar.email@gmail.com>
2021-01-12 21:24:55 -0600

    Added ports/motors as instance fields

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -7,16 +7,33 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 // using WPILib's docs' example from:
 // https://docs.wpilib.org/en/stable/docs/software/examples-tutorials/trajectory-tutorial/creating-drive-subsystem.html
 
 public class DriveSubsystem extends SubsystemBase {
+
+  // port numbers to be added later
+
+  private final static int LEFT_PRIMARY_PORT = 1;
+  private final static int LEFT_SECONDARY_PORT = 2;
+  private final static int RIGHT_PRIMARY_PORT = 3;
+  private final static int RIGHT_SECONDARY_PORT = 4;
+
+  private TalonSRX leftPrimary, leftSecondary, rightPrimary, rightSecondary;
+
   /**
    * Creates a new DriveSubsystem.
    */
   public DriveSubsystem() {
+
+    leftPrimary = new TalonSRX(LEFT_PRIMARY_PORT);
+    leftSecondary = new TalonSRX(LEFT_SECONDARY_PORT);
+    rightPrimary = new TalonSRX(RIGHT_PRIMARY_PORT);
+    rightSecondary = new TalonSRX(RIGHT_SECONDARY_PORT);
 
   }
 

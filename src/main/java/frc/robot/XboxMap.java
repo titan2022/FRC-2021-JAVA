c6194ee4587d499f41ef0ca9591e7d3dbeff02e4
ttchalakov <teodor.tch@gmail.com>
2021-01-17 17:17:33 -0600

    Formatted Tabs and added joystick drift to Xboxmap

--- src/main/java/frc/robot/XboxMap.java
+++ src/main/java/frc/robot/XboxMap.java
@@ -8,30 +8,32 @@ import edu.wpi.first.wpilibj.GenericHID.RumbleType;
  * Xbox button logic definitions
  */
 public class XboxMap {
-    private static final double JOYSTICK_DRIFT = 1e-2;
-    private static final double RUMBLE_INTENSITY = 1; //(0,1]
+  private static final double JOYSTICK_DRIFT = 1e-2;
+  private static final double RUMBLE_INTENSITY = 1; //(0,1]
 
-    private static XboxController controller = Robot.oi.ps4;
+  private static XboxController controller = Robot.oi.ps4;
 
-    //Driving Controls 
+  // Driving Controls 
 	public static double left() {
-		return controller.getY(Hand.kLeft);
-    }
+    double value = controller.getY(Hand.kLeft);
+    return (Math.abs(value) < JOYSTICK_DRIFT) ? 0 : value;
+  }
     
 	public static double right() {
-		return controller.getY(Hand.kRight);
-    }
+    double value = controller.getY(Hand.kRight);
+    return (Math.abs(value) < JOYSTICK_DRIFT) ? 0 : value;
+  }
 
-    public static boolean toggleBrakes() {
-        return controller.getBumperPressed(Hand.kRight);
-    }
+  public static boolean toggleBrakes() {
+    return controller.getBumperPressed(Hand.kRight);
+  }
     
-    public static void startRumble(){
-		controller.setRumble(RumbleType.kLeftRumble, RUMBLE_INTENSITY);
+  public static void startRumble() {
+	  controller.setRumble(RumbleType.kLeftRumble, RUMBLE_INTENSITY);
 		controller.setRumble(RumbleType.kRightRumble, RUMBLE_INTENSITY);
-    }
+  }
     
-	public static void stopRumble(){
+	public static void stopRumble() {
 		controller.setRumble(RumbleType.kLeftRumble, 0);
 		controller.setRumble(RumbleType.kRightRumble, 0);
 	}

9789222f680e3e7e57d138be5f0481e6ed7ca090
blahfacepants <adas@imsa.edu>
2021-01-21 22:08:56 -0600

    idk honestly this was just hanging from earlier

---
+++ src/main/java/frc/robot/kinematics/DifferentialDriveKinematics.java
@@ -0,0 +1,16 @@
+package frc.robot.kinematics;
+
+import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.Robot;
+import 
+
+public class DifferentialDriveKinematics{
+    double breadth, wheelRadius;
+    public DifferentialDriveKinematics(double breadth, double wheelRadius){
+        this.breadth = breadth;
+        this.wheelRadius = wheelRadius;
+    }
+
+    public jacobian()
+}
+

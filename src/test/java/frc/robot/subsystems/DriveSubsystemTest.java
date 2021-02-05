f216d62088458552a05a5ca250e140b5f0bde614
ttchalakov <teodor.tch@gmail.com>
2021-02-05 17:10:45 -0600

    Started drive subsystem unit test

---
+++ src/test/java/frc/robot/subsystems/DriveSubsystemTest.java
@@ -0,0 +1,13 @@
+package frc.robot.subsystems;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class DriveSubsystemTest {
+    @Test
+    void EncoderPositionsTest()
+    {
+        assertEquals("Start this", "Start this");
+    }
+}

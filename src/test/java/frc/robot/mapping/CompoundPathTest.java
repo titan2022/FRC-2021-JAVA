c3116c8b8776c091000fea41e9ec9e539b9c37bd
Irene <ireneicecreamlove@gmail.com>
2021-02-07 14:01:07 -0600

    lin

--- src/test/java/frc/robot/mapping/CompoundPathTest.java
+++ src/test/java/frc/robot/mapping/CompoundPathTest.java
@@ -4,6 +4,6 @@ import org.junit.jupiter.api.Test;
 public class CompoundPathTest {
 
     public void getLengthTest() {
-
+        
     }
 }
--- src/test/java/frc/robot/mapping/LinearSegmentTest.java
+++ src/test/java/frc/robot/mapping/LinearSegmentTest.java
@@ -13,4 +13,14 @@ public class LinearSegmentTest {
         double trueLength = Math.sqrt((1-1) * (1-1) + 2 * 2);
         assertEquals(length, trueLength);
     }
+
+    public void getPosTest()
+    {
+        Point one = new Point (0.5, 0.5*Math.sqrt(3));
+        Point two = new Point (0,0);
+        LinearSegment seg2 = new LinearSegment( two, one);
+        Point truPosition = new Point(5, one.minus(two).getAngle());
+        Point position = seg2.getPos(5.0);
+        assertEquals(position, truPosition);
+    }
 }

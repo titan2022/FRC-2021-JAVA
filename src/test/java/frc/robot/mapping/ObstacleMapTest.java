051328dc8b6d77961ea7f83639be50162255a1ca
Irene <ireneicecreamlove@gmail.com>
2021-02-12 13:55:10 -0600

    obstacle map unit test

--- src/test/java/frc/robot/mapping/ObstacleMapTest.java
+++ src/test/java/frc/robot/mapping/ObstacleMapTest.java
@@ -2,38 +2,51 @@ package frc.robot.mapping;
 
 
 import org.junit.jupiter.api.Test;
-
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+import java.util.function.Consumer;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ObstacleMapTest {
     @Test
     public void isClearTest() {
     }
     @Test
-    public void addObstacleTest() 
-    {
-        CircularArc arc = new CircularArc(new Point(0,1), new Point(0,0), 3.14);
-        double length = arc.getLength();
-        double trueLength = Math.sqrt(1) * 3.14;
-        assertEquals(length, trueLength);
-    }
-
-    public void removeObstacleTest()
-    {
-
-    }
-
     public void getObstaclesTest()
     {
-
+        Obstacle poly = new Polygon(new Point(-2,-2), new Point (-2,2), new Point (2,2), new Point(2,-2));
+        ObstacleMap map = new ObstacleMap();
+        map.addObstacle(poly);
+        Iterable<Obstacle> obs = map.getObstacles();
+        assertEquals(obs, poly);
     }
-    
+    boolean bool = false;
+    void myMethod(Obstacle o) {
+        bool = true;
+    }
+    @Test
     public void onAdditionTest()
     {
-
+        Obstacle poly = new Polygon(new Point(-2,-2), new Point (-2,2), new Point (2,2), new Point(2,-2));
+        ObstacleMap map = new ObstacleMap();
+        boolean add = map.onAddition(this::myMethod);
+        map.addObstacle(poly);
+        assertEquals(bool, true);
     }
 
+    boolean bool2 = false;
+    void myMethod2(Obstacle o) {
+        bool2 = true;
+    }
+    @Test
     public void onRemovalTest()
     {
-        
+        Obstacle poly2 = new Polygon(new Point(-2,-2), new Point (-2,2), new Point (2,2), new Point(2,-2));
+        ObstacleMap map2 = new ObstacleMap();
+        boolean remove = map2.onRemoval(this::myMethod2);
+        map2.addObstacle(poly2);
+        map2.removeObstacle(poly2);
+        assertEquals(bool2, true);
     }
 }

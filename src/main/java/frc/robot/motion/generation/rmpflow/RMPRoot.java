82c8a1fce1d7a41b490466a08b258c92c8537aed
ttchalakov <teodor.tch@gmail.com>
2021-02-11 23:24:55 -0600

    Documented RMP root

--- src/main/java/frc/robot/motion/generation/rmpflow/RMPRoot.java
+++ src/main/java/frc/robot/motion/generation/rmpflow/RMPRoot.java
@@ -4,11 +4,12 @@ import org.ejml.simple.SimpleMatrix;
 
 /**
  * The root node of an RMP tree.
+ * Includes implementation as described in the <a href="https://arxiv.org/abs/1811.07049">RMPFlow Section 3.5</a>.
  */
 public class RMPRoot extends RMPNode{
 	/**
 	 * A root node of an RMP tree.
-	 * @param name of tree
+	 * @param name The name of the tree
 	 */
 	public RMPRoot(String name)
 	{
@@ -17,12 +18,12 @@ public class RMPRoot extends RMPNode{
 	
 	/**
 	 * Updates the states of the tree
-	 * @param x The position
-	 * @param x_dot The velocity
+	 * @param x The state
+	 * @param x_dot The differentiated state
 	 */
 	public void setRootState(SimpleMatrix x, SimpleMatrix x_dot)
 	{
-		if(x.numRows() == 1)
+		if(x.numRows() == 1) //Sets to column matrices
 			setX(x.transpose());
 		else
 			setX(x);
@@ -32,11 +33,6 @@ public class RMPRoot extends RMPNode{
 			setXdot(x_dot);
 	}
 	
-	/**
-	 * 	The operator to forward propagate the state from a parent node to its child nodes.
-	 *  I.e. Transform an RMP from the domain of a task map to its co-domain.
-	 * 	Equation 12 in Riemannian Motion Policies.
-	 */
 	@Override
 	public void pushforward()
 	{
@@ -46,7 +42,7 @@ public class RMPRoot extends RMPNode{
 	
 	/**
 	 * Maps an RMP from its natural form to its canonical form.
-	 * @return A matrix containing the canonical form.
+	 * @return A The matrix containing desired acceleration in canonical form.
 	 */
 	public SimpleMatrix resolve()
 	{
@@ -55,9 +51,9 @@ public class RMPRoot extends RMPNode{
 	
 	/**
 	 * Updates the state of the tree and solves for the desired output.
-	 * @param x The position.
-	 * @param x_dot The velocity.
-	 * @return An matrix representing the resolved state of the RMP tree.
+	 * @param x The state
+	 * @param x_dot The differentiated state
+	 * @return A matrix representing the resolved state of the RMP tree.
 	 */
 	public SimpleMatrix solve(SimpleMatrix x, SimpleMatrix x_dot)
 	{

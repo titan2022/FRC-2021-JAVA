1e3c401680ed4e754f5ee8d2f7c449c8ee1ca2b7
blahfacepants <adas@imsa.edu>
2021-02-13 13:17:26 -0600

    swerve

--- src/main/java/frc/robot/subsystems/SwerveDriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveDriveSubsystem.java
@@ -1,5 +1,7 @@
 package frc.robot.subsystems;
 
+import java.util.ResourceBundle.Control;
+
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRXConfiguration;
@@ -21,6 +23,7 @@ public class SwerveDriveSubsystem extends SubsystemBase
   public static final double ENCODER_TICKS = 4096; // Ticks/rotation of CTREMagEncoder
   public static final double METERS_PER_TICK = WHEEL_RADIUS * 2 * Math.PI / ENCODER_TICKS;
   public static final double GEARING_REDUCTION = 7.29; // TODO: Get the correct gearing ratio
+  public static final double ROBOT_LENGTH = 0.5;
     
   // Port numbers to be added later
   private static final int LEFT_PRIMARY_PORT = 1;
@@ -92,6 +95,25 @@ public class SwerveDriveSubsystem extends SubsystemBase
   private static final double LEFT_POS_MEAS_NOISE = 0.005; // meter
   private static final double RIGHT_POS_MEAS_NOISE = 0.005; // meter
 
+  //PID for rotators
+  private static final int ROTATOR_TIMEOUT = 30; //ms
+  private static final double ROTATOR_VELOCITY = 1000;
+  private static final double ROTATOR_ACCELERATION = 1000;
+  private static final int LEFT_PRIMARY_ROTATOR_SLOT_IDX = 0;
+  private static final int LEFT_PRIMARY_ROTATOR_PID_IDX = 0;
+  private static final int RIGHT_PRIMARY_ROTATOR_SLOT_IDX = 0;
+  private static final int RIGHT_PRIMARY_ROTATOR_PID_IDX = 0;
+  private static final int LEFT_SECONDARY_ROTATOR_SLOT_IDX = 0;
+  private static final int LEFT_SECONDARY_ROTATOR_PID_IDX = 0;
+  private static final int RIGHT_SECONDARY_ROTATOR_SLOT_IDX = 0;
+  private static final int RIGHT_SECONDARY_ROTATOR_PID_IDX = 0;
+  private static final double ROTATOR_KF = 0.5;
+  private static final double ROTATOR_KP = 0.02;
+  private static final double ROTATOR_KI = 0;
+  private static final double ROTATOR_KD = 0.01;
+
+
+
   /**
    * Creates a new SwerveSubsystem.
    */
@@ -117,12 +139,6 @@ public class SwerveDriveSubsystem extends SubsystemBase
     leftPrimaryRotator.setInverted(LEFT_PRIMARY_ROTATOR_INVERTED);
     leftSecondaryRotator.setInverted(LEFT_SECONDARY_ROTATOR_ARY_INVERTED);
 
-    //Add if rotators follow
-    leftSecondary.follow(leftPrimary);
-    rightSecondary.follow(rightPrimary);
-    leftSecondaryRotator.follow(leftPrimaryRotator);
-    rightSecondaryRotator.follow(rightPrimaryRotator);
-
     // Sets the direction that the talon will turn on the green LED when going 'forward'.
     leftPrimary.setSensorPhase(LEFT_PRIMARY_MOTOR_SENSOR_PHASE);
     rightPrimary.setSensorPhase(RIGHT_PRIMARY_MOTOR_SENSOR_PHASE);
@@ -142,6 +158,30 @@ public class SwerveDriveSubsystem extends SubsystemBase
     Faults _faults_L = new Faults();
     Faults _faults_R = new Faults();
     */
+
+    leftPrimaryRotator.selectProfileSlot(LEFT_PRIMARY_ROTATOR_SLOT_IDX, LEFT_PRIMARY_ROTATOR_PID_IDX);
+    leftPrimaryRotator.config_kF(LEFT_PRIMARY_ROTATOR_SLOT_IDX, ROTATOR_KF, ROTATOR_TIMEOUT);
+    leftPrimaryRotator.config_kP(LEFT_PRIMARY_ROTATOR_SLOT_IDX, ROTATOR_KP, ROTATOR_TIMEOUT);
+    leftPrimaryRotator.config_kI(LEFT_PRIMARY_ROTATOR_SLOT_IDX, ROTATOR_KI, ROTATOR_TIMEOUT);
+    leftPrimaryRotator.config_kD(LEFT_PRIMARY_ROTATOR_SLOT_IDX, ROTATOR_KD, ROTATOR_TIMEOUT);
+
+    rightPrimaryRotator.selectProfileSlot(RIGHT_PRIMARY_ROTATOR_SLOT_IDX, RIGHT_PRIMARY_ROTATOR_PID_IDX);
+    rightPrimaryRotator.config_kF(RIGHT_PRIMARY_ROTATOR_SLOT_IDX, ROTATOR_KF, ROTATOR_TIMEOUT);
+    rightPrimaryRotator.config_kP(RIGHT_PRIMARY_ROTATOR_SLOT_IDX, ROTATOR_KP, ROTATOR_TIMEOUT);
+    rightPrimaryRotator.config_kI(RIGHT_PRIMARY_ROTATOR_SLOT_IDX, ROTATOR_KI, ROTATOR_TIMEOUT);
+    rightPrimaryRotator.config_kD(RIGHT_PRIMARY_ROTATOR_SLOT_IDX, ROTATOR_KD, ROTATOR_TIMEOUT);
+
+    leftSecondaryRotator.selectProfileSlot(LEFT_SECONDARY_ROTATOR_SLOT_IDX, LEFT_SECONDARY_ROTATOR_PID_IDX);
+    leftSecondaryRotator.config_kF(LEFT_SECONDARY_ROTATOR_SLOT_IDX, ROTATOR_KF, ROTATOR_TIMEOUT);
+    leftSecondaryRotator.config_kP(LEFT_SECONDARY_ROTATOR_SLOT_IDX, ROTATOR_KP, ROTATOR_TIMEOUT);
+    leftSecondaryRotator.config_kI(LEFT_SECONDARY_ROTATOR_SLOT_IDX, ROTATOR_KI, ROTATOR_TIMEOUT);
+    leftSecondaryRotator.config_kD(LEFT_SECONDARY_ROTATOR_SLOT_IDX, ROTATOR_KD, ROTATOR_TIMEOUT);
+
+    rightSecondaryRotator.selectProfileSlot(RIGHT_SECONDARY_ROTATOR_SLOT_IDX, RIGHT_SECONDARY_ROTATOR_PID_IDX);
+    rightSecondaryRotator.config_kF(RIGHT_SECONDARY_ROTATOR_SLOT_IDX, ROTATOR_KF, ROTATOR_TIMEOUT);
+    rightSecondaryRotator.config_kP(RIGHT_SECONDARY_ROTATOR_SLOT_IDX, ROTATOR_KP, ROTATOR_TIMEOUT);
+    rightSecondaryRotator.config_kI(RIGHT_SECONDARY_ROTATOR_SLOT_IDX, ROTATOR_KI, ROTATOR_TIMEOUT);
+    rightSecondaryRotator.config_kD(RIGHT_SECONDARY_ROTATOR_SLOT_IDX, ROTATOR_KD, ROTATOR_TIMEOUT);
   }
 
   //Might need extra parameters for rotator motors
@@ -172,24 +212,40 @@ public class SwerveDriveSubsystem extends SubsystemBase
    * @param leftOutputValue  left side output value for ControlMode
    * @param rightOutputValue right side output value for ControlMode
    */
-  public void setOutput(ControlMode mode, double leftOutputValue, double rightOutputValue) {
+  public void setOutput(ControlMode mode, double Omega, double XVelocity, double YVelocity) {
+    double leftPrimaryOutput, leftSecondaryOutput, leftPrimaryRotatorSetpoint, leftSecondaryRotatorSetpoint,
+      rightPrimaryOutput, rightSecondaryOutput, rightPrimaryRotatorSetpoint, rightSecondaryRotatorSetpoint;
+    // TODO: if check the current usage from Power Subsystem to restrict overcurrent
+    // TODO: make this not fucking suck lol
+    // TODO: make the controlmode output correspond to actual wheel velocity in m/s
 
-    if (mode == ControlMode.Velocity) {
+    double A, B, C, D;
 
-      if (leftOutputValue > MAX_SPEED) {
-        leftOutputValue = MAX_SPEED;
-      }
+    A=XVelocity-(Omega * (ROBOT_LENGTH/2));
+    B=XVelocity+(Omega * (ROBOT_LENGTH/2));
+    C=XVelocity-(Omega * (ROBOT_TRACK_WIDTH/2));
+    D=XVelocity+(Omega * (ROBOT_TRACK_WIDTH/2));
 
-      if (rightOutputValue > MAX_SPEED) {
-        rightOutputValue = MAX_SPEED;
-      }
+    leftPrimaryOutput = Math.sqrt((B*B)+(D*D));
+    rightPrimaryOutput = Math.sqrt((B*B)+(C*C));
+    leftSecondaryOutput = Math.sqrt((A*A)+(D*D));
+    rightSecondaryOutput = Math.sqrt((A*A)+(C*C));
 
-    }
+    leftPrimaryRotatorSetpoint = Math.atan2(B, D)*(180/Math.PI);
+    rightPrimaryRotatorSetpoint = Math.atan2(B, C)*(180/Math.PI);
+    leftSecondaryRotatorSetpoint = Math.atan2(A, D)*(180/Math.PI);
+    rightSecondaryRotatorSetpoint = Math.atan2(A, C)*(180/Math.PI);
 
-    // TODO: is check the current usage from Power Subsystem to restrict overcurrent
-    leftPrimary.set(mode, leftOutputValue);
-    rightPrimary.set(mode, rightOutputValue);
-  }
+    leftPrimary.set(mode, leftPrimaryOutput);
+    rightPrimary.set(mode, rightPrimaryOutput);
+    leftSecondary.set(mode, leftSecondaryOutput);
+    rightSecondary.set(mode, rightSecondaryOutput);
+
+    leftPrimaryRotator.set(ControlMode.MotionMagic, leftPrimaryRotatorSetpoint);
+    rightPrimaryRotator.set(ControlMode.MotionMagic, rightPrimaryRotatorSetpoint);
+    leftSecondaryRotator.set(ControlMode.MotionMagic, leftSecondaryRotatorSetpoint);
+    rightSecondaryRotator.set(ControlMode.MotionMagic, rightSecondaryRotatorSetpoint);
+  } 
 
   /**
    * Enables brake.

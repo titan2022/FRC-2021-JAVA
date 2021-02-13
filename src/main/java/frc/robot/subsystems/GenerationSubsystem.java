9e92577610dbd1f040e3c8063d38e191d4718092
rajpatkar <rajan.patkar.email@gmail.com>
2021-02-13 13:18:14 -0600

    Added GenerationSubsystem constructors

--- src/main/java/frc/robot/subsystems/GenerationSubsystem.java
+++ src/main/java/frc/robot/subsystems/GenerationSubsystem.java
@@ -4,14 +4,56 @@
 
 package frc.robot.subsystems;
 
+import java.util.ArrayList;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.motion.generation.rmpflow.CollisionAvoidance;
+import frc.robot.motion.generation.rmpflow.GoalAttractor;
+import frc.robot.motion.generation.rmpflow.RMPRoot;
 
 public class GenerationSubsystem extends SubsystemBase {
+
+  private RMPRoot root = new RMPRoot("root");
+  private ArrayList<CollisionAvoidance> collisionAvoiders;
+  private ArrayList<GoalAttractor> goalAttractors;
+
   /** Creates a new GenerationSubsystem. */
   public GenerationSubsystem() {
 
-
+    collisionAvoiders = new ArrayList<CollisionAvoidance>();
+    goalAttractors = new ArrayList<GoalAttractor>();
     
+  }
+
+  public GenerationSubsystem(ArrayList<CollisionAvoidance> collisionAvoiders, ArrayList<GoalAttractor> goalAttractors) {
+
+    setCollisionAvoiders(collisionAvoiders);
+    setGoalAttractors(goalAttractors);
+
+  }
+
+  public void setCollisionAvoiders(ArrayList<CollisionAvoidance> collisionAvoiders) {
+
+    this.collisionAvoiders = collisionAvoiders;
+
+  }
+
+  public void setGoalAttractors(ArrayList<GoalAttractor> goalAttractors) {
+
+    this.goalAttractors = goalAttractors;
+
+  }
+
+  public void addGoalAttractor(GoalAttractor goalAttractor) {
+
+    goalAttractors.add(goalAttractor);
+
+  }
+
+  public void addCollisionAvoider(CollisionAvoidance collisionAvoider) {
+
+    collisionAvoiders.add(collisionAvoider);
+  
   }
 
   @Override

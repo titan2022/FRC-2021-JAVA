5996a987ecb069d283ffb3f760973435183dfb5d
ttchalakov <teodor.tch@gmail.com>
2021-02-14 14:53:32 -0600

    Added shuffleboard widget dependencies

---
+++ src/main/java/frc/robot/shuffleboardplugins/MyPlugin.java
@@ -0,0 +1,9 @@
+package frc.robot.shuffleboardplugins;
+
+import edu.wpi.first.shuffleboard.api.plugin.Description;
+import edu.wpi.first.shuffleboard.api.plugin.Plugin;
+
+@Description(group = "com.example", name = "MyPlugin", version = "1.2.3", summary = "An example plugin")
+public class MyPlugin extends Plugin {
+
+}

6a03ba3762fd1c08959bb734bc015efc16b9004a
rajpatkar <rajan.patkar.email@gmail.com>
2021-02-14 12:06:50 -0600

    Added getParent to RMPNode for use in MotionGenerationSubsystem

--- src/main/java/frc/robot/motion/generation/rmpflow/RMPNode.java
+++ src/main/java/frc/robot/motion/generation/rmpflow/RMPNode.java
@@ -84,6 +84,15 @@ public abstract class RMPNode {
 	{
 		return children;
 	}
+
+	/**
+	 * Returns parent node.
+	 * @return Parent node.
+	 */
+	public RMPNode getParent()
+	{
+		return parent;
+	}
 	
 	/**
 	 * 	The operator to forward propagate the state from a parent node to its child nodes.

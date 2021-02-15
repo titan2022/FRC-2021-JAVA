f1fa9b3c1c24ca8467237dbd4ff0e5d0f6882e25
ttchalakov <teodor.tch@gmail.com>
2021-02-15 11:33:17 -0600

    made controllers static

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -8,10 +8,10 @@ import edu.wpi.first.wpilibj.XboxController;
  */
 public final class OI {
     // Controller OI
-    public final XboxController ps4 = new XboxController(PS4_PORT), xbox = new XboxController(XBOX_PORT);;
     private static final int PS4_PORT = 0, XBOX_PORT = 1; // Drive station ports will be different from simulator ports
+    public static final XboxController ps4 = new XboxController(PS4_PORT), xbox = new XboxController(XBOX_PORT);
 
     // Control Panel OI
-    public final Joystick xinmotek1 = new Joystick(XINMOTEK1_PORT), xinmotek2 = new Joystick(XINMOTEK2_PORT);
     private static final int XINMOTEK1_PORT = 2, XINMOTEK2_PORT = 3;
+    public static final Joystick xinmotek1 = new Joystick(XINMOTEK1_PORT), xinmotek2 = new Joystick(XINMOTEK2_PORT);
 }

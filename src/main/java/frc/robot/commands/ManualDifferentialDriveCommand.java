a1654da76cb64284affe33fe956bbf0ccd746b0d
ttchalakov <teodor.tch@gmail.com>
2021-02-15 20:00:34 -0600

    Fixed imports

--- src/main/java/frc/robot/commands/ManualDifferentialDriveCommand.java
+++ src/main/java/frc/robot/commands/ManualDifferentialDriveCommand.java
@@ -7,7 +7,7 @@ package frc.robot.commands;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.XboxMap;
+import frc.robot.config.XboxMap;
 import frc.robot.subsystems.DifferentialDriveSubsystem;
 
 /**

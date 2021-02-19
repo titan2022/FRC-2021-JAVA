021c39a3cec96ff04fcd251ac9c32643b24103a8
ttchalakov <teodor.tch@gmail.com>
2021-02-18 19:52:25 -0600

    Cleaned up differential drive odometry

--- src/main/java/frc/robot/commands/DifferentialDriveOdometryCommand.java
+++ src/main/java/frc/robot/commands/DifferentialDriveOdometryCommand.java
@@ -1,119 +1,99 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.commands;
+
+import frc.robot.subsystems.DifferentialDriveSubsystem;
+import frc.robot.subsystems.NavigationSubsystem;
 
 import org.ejml.simple.SimpleMatrix;
 
 import edu.wpi.first.wpilibj.geometry.Pose2d;
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.kinematics.DifferentialDriveOdometry;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.subsystems.DifferentialDriveSubsystem;
-import frc.robot.subsystems.NavigationSubsystem;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
+/**
+ * 
+ */
 public class DifferentialDriveOdometryCommand extends CommandBase {
-
-  // ALL ODOMETRY DONE IN METERS, not imperial
-
+  // ALL ODOMETRY IS DONE IN METERS, NOT IMPERIAL!!!
   private final DifferentialDriveSubsystem driveSub;
   private final NavigationSubsystem navSub;
   private final DifferentialDriveOdometry odometry;
-  private boolean useFieldSim;
-  private Field2d fieldSim;
+
+  private Field2d fieldDisplay;
+  private boolean useFieldDisplay;
 
   /**
    * Creates a new OdometryCommand without a field simulation.
-   * 
    * @param driveSub - Differential drive subsystem.
    * @param navSub   - Navigation subsystem.
    */
   public DifferentialDriveOdometryCommand(DifferentialDriveSubsystem driveSub, NavigationSubsystem navSub) {
-    
-    // Use addRequirements() here to declare subsystem dependencies.
     this.driveSub = driveSub;
     this.navSub = navSub;
     odometry = new DifferentialDriveOdometry(navSub.getHeadingRotation2d());
-    useFieldSim = false;
-
+    useFieldDisplay = false;
   }
 
   /**
    * Creates a new OdometryCommand with a field simulation.
-   * 
    * @param driveSub - Differential drive subsystem.
    * @param navSub   - Navigation subsystem.
-   * @param fieldSim - Field2d to be simulated.
+   * @param useFieldDisplay - Field2d to be simulated.
    */
-  public DifferentialDriveOdometryCommand(DifferentialDriveSubsystem driveSub, NavigationSubsystem navSub, Field2d fieldSim) {
+  public DifferentialDriveOdometryCommand(DifferentialDriveSubsystem driveSub, NavigationSubsystem navSub, boolean useFieldDisplay) {
+    this(driveSub, navSub);    
+    this.useFieldDisplay = useFieldDisplay;
 
-    this(driveSub, navSub);
-    this.fieldSim = fieldSim;
-    useFieldSim = true;
-    
+    if(useFieldDisplay) {
+      this.fieldDisplay = new Field2d();
+    }
   }
 
-  // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-
     System.out.println("Differential drive odometry command initialized.");
-
   }
 
-  // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-
     updateOdometry();
 
-    if (useFieldSim) updateFieldSim();
-
+    if (useFieldDisplay) updateFieldSim();
   }
 
-  // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+  }
 
-  // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-
     return false;
-
   }
 
-  // odometry methods
+  // Odometry Methods
 
   /**
    * Gets current DifferentialDriveOdometry.
-   * 
    * @return Differential drive odometry.
    */
   public DifferentialDriveOdometry getOdometry() {
-
     return odometry;
-
   }
 
   /**
    * Updates differential drive odometry.
    */
   private void updateOdometry() {
-
     odometry.update(navSub.getHeadingRotation2d(), driveSub.getEncoderDist(true), driveSub.getEncoderDist(false));
-
   }
 
   /**
    * Resets differential drive odometry.
    */
-  public void resetOdometry() {
-
-    odometry.resetPosition(new Pose2d(getX(), getY(), navSub.getHeadingRotation2d()), navSub.getHeadingRotation2d());
-
+  public void resetOdometry(Pose2d poseMeters, Rotation2d gyroAngle) {
+    odometry.resetPosition(poseMeters, gyroAngle);
   }
 
   /**
@@ -121,9 +101,7 @@ public class DifferentialDriveOdometryCommand extends CommandBase {
    * @return X (meters).
    */
   public double getX() {
-
     return odometry.getPoseMeters().getX();
-
   }
 
   /**
@@ -131,20 +109,23 @@ public class DifferentialDriveOdometryCommand extends CommandBase {
    * @return Y (meters).
    */
   public double getY() {
-
     return odometry.getPoseMeters().getY();
+  }
 
+  /**
+   * Gets the pose in meters
+   * @return Pose2d object with x, y, and theta
+   */
+  public Pose2d getPose() {
+    return odometry.getPoseMeters();
   }
 
   /**
    * Gets filterable vector motion measurement from odometry.
-   * 
    * @return Odometry vector measurement.
    */
   public SimpleMatrix getOdometryVector() {
-
-    return new SimpleMatrix(new double[][] { { getX() }, { 0 }, { 0 }, { getY() }, { 0 }, { 0 } });
-
+    return new SimpleMatrix(new double[][] { { getX() }, { 0 }, { 0 }, { getY() }, { 0 }, { 0 } }); // TODO: remove the velocity, acceleration and replace with x, y, theta
   }
 
   // simulation methods
@@ -152,11 +133,8 @@ public class DifferentialDriveOdometryCommand extends CommandBase {
   /**
    * Updates field simulation
    */
-  private void updateFieldSim() {
-
+  private void updateFieldSim() { // TODO: Move this out of here
     fieldSim.setRobotPose(odometry.getPoseMeters().getX(), odometry.getPoseMeters().getY(), navSub.getHeadingRotation2d());
     SmartDashboard.putData("Field", fieldSim);
-
   }
-
 }

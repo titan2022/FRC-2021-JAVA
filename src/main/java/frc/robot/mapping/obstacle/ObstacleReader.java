861990de487dd3ae7d8652c4a857e36064b77e33
rajpatkar <rajan.patkar.email@gmail.com>
2021-02-24 20:56:59 -0600

    Finished logic for getting ObstacleMap from csv

--- src/main/java/frc/robot/mapping/obstacle/ObstacleReader.java
+++ src/main/java/frc/robot/mapping/obstacle/ObstacleReader.java
@@ -10,6 +10,11 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.io.File;
+import frc.robot.mapping.Polygon;
+import frc.robot.mapping.ObstacleMap;
+import frc.robot.mapping.Point;
+import java.util.ArrayList;
+import static java.lang.Double.parseDouble;
 
 import edu.wpi.first.wpilibj.Filesystem;
 
@@ -18,20 +23,33 @@ public class ObstacleReader {
 
     public static String OBSTACLE_FILE_NAME = "obstacles.csv";
 
-    public static void read() throws FileNotFoundException, IOException {
+    public static ObstacleMap read() throws FileNotFoundException, IOException {
 
+        ObstacleMap map = new ObstacleMap();
         Path csvPath = Filesystem.getDeployDirectory().toPath().resolve(OBSTACLE_FILE_NAME);
         BufferedReader reader = new BufferedReader(new FileReader(new File(csvPath.toString())));
         String line;
+        ArrayList<Point> points = new ArrayList<Point>();
 
         while ((line = reader.readLine()) != null) {
 
-            System.out.println(line);
+            if (line == ",") {
+
+                Point[] pointsArray = points.toArray(new Point[points.size()]);
+                map.addObstacle(new Polygon(pointsArray));
+
+            } else {
+
+                String[] coordStrings = line.split(",");
+                points.add(new Point(parseDouble(coordStrings[0]), parseDouble(coordStrings[1])));
+
+            }
 
         }
 
         reader.close();
+        return map;
 
     }
-    
+
 }

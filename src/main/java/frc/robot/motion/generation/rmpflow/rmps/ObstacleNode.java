d24ceae339fd238c3a1c7f43861b3e156467fe1e
rajpatkar <rajan.patkar.email@gmail.com>
2021-02-26 21:00:42 -0600

    Added addObstacleChildren

--- src/main/java/frc/robot/motion/generation/rmpflow/rmps/ObstacleNode.java
+++ src/main/java/frc/robot/motion/generation/rmpflow/rmps/ObstacleNode.java
@@ -4,30 +4,60 @@
 
 package frc.robot.motion.generation.rmpflow.rmps;
 
+import org.ejml.simple.SimpleMatrix;
+
 import frc.robot.mapping.Obstacle;
 import frc.robot.mapping.ObstacleMap;
+import frc.robot.mapping.Path;
+import frc.robot.mapping.Point;
 import frc.robot.motion.generation.rmpflow.RMPNode;
+import frc.robot.subsystems.DifferentialDriveSubsystem;
 
 /**
  * ObstacleNode works with field obstacles.
  */
 public class ObstacleNode extends RMPNode {
 
-    public ObstacleNode(ObstacleMap map, RMPNode parent) {
+    private final static double RADIUS = DifferentialDriveSubsystem.ROBOT_TRACK_WIDTH / 2;
+    private final static double epsilon = 1e-1;
+    private final static double alpha = 1e-5;
+    private final static double eta = 0;
 
-        super("Obstacle Node", parent);
+    public ObstacleNode(String name, ObstacleMap map, RMPNode parent) {
+
+        super(name, parent);
+        addObstacleChildren(map);
 
     }
 
-    private addObstacleChildren(ObstacleMap map) {
+    private void addObstacleChildren(ObstacleMap map) {
 
-        Iterable<Obstacle> obstacles = map.getObstacles();      
+        Iterable<Obstacle> obstacles = map.getObstacles();
+        Path path;
+        int nodeCount;
+        int i;
 
         for (Obstacle obstacle : obstacles) {
 
-            
+            path = obstacle.getBoundary();
+            nodeCount = (int) (path.getLength() / RADIUS);
+
+            for (i = 0; i < nodeCount; i++) {
+
+                linkChildFromPoint(path.getPos(i * RADIUS));
+
+            }
+
+            linkChildFromPoint(path.getEnd());
 
         }
+
+    }
+
+    private void linkChildFromPoint(Point point) {
+
+        linkChild(new CollisionAvoidance("Child Obstacle Node", this,
+                new SimpleMatrix(new double[][] { { point.getX(), point.getY() } }), RADIUS, epsilon, alpha, eta));
 
     }
 

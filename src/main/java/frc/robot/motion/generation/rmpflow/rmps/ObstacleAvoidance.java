82cf6a468732bc436a2aafed0796e636da769142
rajpatkar <rajan.patkar.email@gmail.com>
2021-02-27 13:15:28 -0600

    Renamed to ObstacleAvoidance

--- src/main/java/frc/robot/motion/generation/rmpflow/rmps/ObstacleAvoidance.java
+++ src/main/java/frc/robot/motion/generation/rmpflow/rmps/ObstacleAvoidance.java
@@ -10,20 +10,21 @@ import frc.robot.mapping.Obstacle;
 import frc.robot.mapping.ObstacleMap;
 import frc.robot.mapping.Path;
 import frc.robot.mapping.Point;
+import frc.robot.mapping.Polygon;
 import frc.robot.motion.generation.rmpflow.RMPNode;
 import frc.robot.subsystems.DifferentialDriveSubsystem;
 
 /**
  * ObstacleNode works with field obstacles.
  */
-public class ObstacleNode extends RMPNode {
+public class ObstacleAvoidance extends RMPNode {
 
     private final static double RADIUS = DifferentialDriveSubsystem.ROBOT_TRACK_WIDTH / 2;
     private final static double epsilon = 1e-1;
     private final static double alpha = 1e-5;
     private final static double eta = 0;
 
-    public ObstacleNode(String name, ObstacleMap map, RMPNode parent) {
+    public ObstacleAvoidance(String name, ObstacleMap map, RMPNode parent) {
 
         super(name, parent);
         addObstacleChildren(map);

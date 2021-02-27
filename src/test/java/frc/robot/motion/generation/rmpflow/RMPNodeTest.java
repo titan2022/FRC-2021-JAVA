7a17fd1afbb432796cc8c7f8a90d653f305f1125
rajpatkar <rajan.patkar.email@gmail.com>
2021-02-26 19:37:24 -0600

    Added linkChildTest and unlinkChildTest

--- src/test/java/frc/robot/motion/generation/rmpflow/RMPNodeTest.java
+++ src/test/java/frc/robot/motion/generation/rmpflow/RMPNodeTest.java
@@ -22,7 +22,7 @@ public class RMPNodeTest {
         policy2 = new CollisionAvoidance("Collision Avoidance Policy 2", parent1, new SimpleMatrix(1, 2,  false, new double[] {0, 0}), 1.0, .2, 1e-5, 0.0);
     }
     @Test
-    void linkToParentTest()
+    void linkParentTest()
     {
         policy1.linkParent(parent2);
         assertEquals(1, parent1.getChildren().size());
@@ -34,7 +34,7 @@ public class RMPNodeTest {
         assertTrue(foundChild);
     }
     @Test
-    void unlinkToParentTest()
+    void unlinkParentTest()
     {
         policy1.unlinkParent(parent1);
         assertEquals(1, parent1.getChildren().size());
@@ -45,4 +45,31 @@ public class RMPNodeTest {
         }
         assertTrue(!foundChild);
     }
+
+    @Test
+    void linkChildTest()
+    {
+        parent2.linkChild(policy1);
+        assertEquals(1, parent2.getChildren().size());
+        assertEquals(parent2, policy1.getParent());
+        boolean foundChild = false;
+        for (var child : parent2.getChildren()) {
+            if (child == policy1) foundChild = true;
+        }
+        assertTrue(foundChild);
+    }
+
+    @Test
+    void unlinkChildTest()
+    {
+        parent1.unlinkChild(policy2);
+        assertEquals(1, parent1.getChildren().size());
+        assertEquals(null, policy2.getParent());
+        boolean foundChild = false;
+        for (var child : parent2.getChildren()) {
+            if (child == policy2) foundChild = true;
+        }
+        assertTrue(!foundChild);
+    }
+
 }

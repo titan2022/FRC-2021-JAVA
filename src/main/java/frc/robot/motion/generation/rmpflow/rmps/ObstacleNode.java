a3a6e7576737098344b6d643b0773476632aa044
ttchalakov <teodor.tch@gmail.com>
2021-02-27 13:21:47 -0600

    Formatted Obstacle RMP

--- src/main/java/frc/robot/motion/generation/rmpflow/rmps/ObstacleNode.java
+++ src/main/java/frc/robot/motion/generation/rmpflow/rmps/ObstacleNode.java
@@ -11,54 +11,53 @@ import frc.robot.mapping.ObstacleMap;
 import frc.robot.mapping.Path;
 import frc.robot.mapping.Point;
 import frc.robot.motion.generation.rmpflow.RMPNode;
-import frc.robot.subsystems.DifferentialDriveSubsystem;
 
 /**
  * ObstacleNode works with field obstacles.
  */
 public class ObstacleNode extends RMPNode {
+    private double radius;
+    private double epsilon = 1e-1;
+    private double alpha = 1e-5;
+    private double eta = 0;
 
-    private final static double RADIUS = DifferentialDriveSubsystem.ROBOT_TRACK_WIDTH / 2;
-    private final static double epsilon = 1e-1;
-    private final static double alpha = 1e-5;
-    private final static double eta = 0;
-
-    public ObstacleNode(String name, ObstacleMap map, RMPNode parent) {
-
+    /**
+     * An Obstacle avoidance RMP that places {@link CollisionAvoidance} RMPs at all
+     * polygon vertices and regularly along the sides of the polygon with a distance
+     * radius.
+     * @param name
+     * @param map
+     * @param parent
+     * @param radius
+     */
+    public ObstacleNode(String name, ObstacleMap map, RMPNode parent, double radius) {
         super(name, parent);
         addObstacleChildren(map);
-
+        this.radius = radius;
     }
 
     private void addObstacleChildren(ObstacleMap map) {
-
         Iterable<Obstacle> obstacles = map.getObstacles();
         Path path;
         int nodeCount;
         int i;
 
         for (Obstacle obstacle : obstacles) {
-
             path = obstacle.getBoundary();
-            nodeCount = (int) (path.getLength() / RADIUS);
+            nodeCount = (int) (path.getLength() / radius);
 
             for (i = 0; i < nodeCount; i++) {
 
-                linkChildFromPoint(path.getPos(i * RADIUS));
+                linkChildFromPoint(path.getPos(i * radius));
 
             }
 
             linkChildFromPoint(path.getEnd());
-
         }
-
     }
 
     private void linkChildFromPoint(Point point) {
-
         linkChild(new CollisionAvoidance("Child Obstacle Node", this,
-                new SimpleMatrix(new double[][] { { point.getX(), point.getY() } }), RADIUS, epsilon, alpha, eta));
-
+                new SimpleMatrix(new double[][] { { point.getX(), point.getY() } }), radius, epsilon, alpha, eta));
     }
-
 }

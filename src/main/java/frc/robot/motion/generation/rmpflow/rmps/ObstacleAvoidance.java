8f20c55c5c3febed61bd474b09919e3fbd31c76d
rajpatkar <rajan.patkar.email@gmail.com>
2021-03-02 16:25:41 -0600

    Added temporary static nested class for testing

--- src/main/java/frc/robot/motion/generation/rmpflow/rmps/ObstacleAvoidance.java
+++ src/main/java/frc/robot/motion/generation/rmpflow/rmps/ObstacleAvoidance.java
@@ -7,6 +7,7 @@ import frc.robot.mapping.ObstacleMap;
 import frc.robot.mapping.Point;
 import frc.robot.mapping.Polygon;
 import frc.robot.motion.generation.rmpflow.RMPNode;
+import frc.robot.motion.generation.rmpflow.RMPRoot;
 
 /**
  * ObstacleNode works with field obstacles.
@@ -114,5 +115,68 @@ public class ObstacleAvoidance extends RMPNode {
 	public SimpleMatrix j_dot(SimpleMatrix q, SimpleMatrix q_dot)
 	{
 		return new SimpleMatrix(2, 3); //TODO: verify if this is the correct j_dot function
-	}
+    }
+    
+    // TEMPORARY
+    // TODO: REMOVE TEMPORARY CLASS
+    public static class ObstacleAvoidanceStatic {
+
+        public static void addRootObstacleChildren(ObstacleMap map, RMPRoot root, double radius) {
+
+            Iterable<Obstacle> obstacles = map.getObstacles();
+    
+            for (Obstacle obstacle : obstacles) {
+    
+                placePolygonPoints(((Polygon) obstacle).getVertices(), root, radius);
+    
+            }
+        }
+    
+        private static void linkChildFromPoint(Point point, RMPRoot root, double radius) {
+            root.linkChild(new CollisionAvoidance("Child of Obstacle Avoidance Node", root,
+                    new SimpleMatrix(1, 2, false, new double[] { point.getX(), point.getY() }), radius, 0.2, 1e-5, 2));
+        }
+
+        private static void placePolygonPoints(Point[] vertexArray, RMPRoot root, double radius) {
+
+            for (int i = 0; i < vertexArray.length - 1; i++) {
+    
+                linkChildFromPoint(vertexArray[i], root, radius);
+                placeSegmentPoints(vertexArray[i], vertexArray[i + 1], root, radius);
+    
+            }
+    
+            linkChildFromPoint(vertexArray[vertexArray.length - 1], root, radius);
+            placeSegmentPoints(vertexArray[vertexArray.length - 1], vertexArray[0], root, radius);
+    
+        }
+    
+        private static void placeSegmentPoints(Point point1, Point point2, RMPRoot root, double radius) {
+    
+            Point midpoint = midpoint(point1, point2);
+            linkChildFromPoint(midpoint, root, radius);
+    
+            if (distance(point1, point2) > radius) {
+    
+                placeSegmentPoints(point1, midpoint, root, radius);
+                placeSegmentPoints(midpoint, point2, root, radius);
+    
+            }
+    
+        }
+
+        private static Point midpoint(Point point1, Point point2) {
+
+            return point1.plus(point2).div(2);
+    
+        }
+    
+        private static double distance(Point point1, Point point2) {
+    
+            return point1.minus(point2).getNorm();
+    
+        }
+
+    }
+
 }

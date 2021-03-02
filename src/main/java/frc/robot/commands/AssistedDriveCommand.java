1f844c25902869d65edf07857d71b732be008e1c
cplepel-py <cbplepel@gmail.com>
2021-03-01 22:46:24 -0600

    Rewrote AssistedDriveCommand to accelerate to input vel

--- src/main/java/frc/robot/commands/AssistedDriveCommand.java
+++ src/main/java/frc/robot/commands/AssistedDriveCommand.java
@@ -6,9 +6,6 @@ import org.ejml.simple.SimpleMatrix;
 
 import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.geometry.Pose2d;
-import edu.wpi.first.wpilibj.kinematics.ChassisSpeeds;
-import edu.wpi.first.wpilibj.kinematics.DifferentialDriveKinematics;
-import edu.wpi.first.wpilibj.kinematics.DifferentialDriveWheelSpeeds;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.config.XboxMap;
 import frc.robot.motion.generation.rmpflow.RMPRoot;
@@ -18,11 +15,10 @@ import frc.robot.subsystems.DifferentialDriveSubsystem;
 public class AssistedDriveCommand extends CommandBase {
     public final DifferentialDriveFilterCommand filter;
     public final DifferentialDriveSubsystem drive;
-    private final DifferentialDriveKinematics kinematics =
-        new DifferentialDriveKinematics(DifferentialDriveSubsystem.ROBOT_TRACK_WIDTH);
     private final RMPRoot root = new RMPRoot("Assisted Driving");
     private long t0 = 0;
     private double lambda = 0;
+    private SimpleMatrix prev = new SimpleMatrix(new double[][]{{0}, {0}, {0}});
 
     public AssistedDriveCommand(DifferentialDriveSubsystem drive, DifferentialDriveFilterCommand filter, ObstacleAvoidance obstacleRMP, double lambda) {
         this.filter = filter;
@@ -37,26 +33,32 @@ public class AssistedDriveCommand extends CommandBase {
 
     @Override
     public void execute() {
-        DifferentialDriveWheelSpeeds ipt = new DifferentialDriveWheelSpeeds(XboxMap.left()*10, XboxMap.right()*10);
-        ChassisSpeeds ipt_vel = kinematics.toChassisSpeeds(ipt);
+        double target_left = XboxMap.left()*10;
+        double target_right = XboxMap.right()*10;
+        double speed = (target_left + target_right) / 2;
         Pose2d pose = filter.getFilteredPose();
-        double vx0 = ipt_vel.vxMetersPerSecond * pose.getRotation().getCos() +
-            ipt_vel.vyMetersPerSecond * pose.getRotation().getSin();
-        double vy0 = ipt_vel.vxMetersPerSecond * pose.getRotation().getCos() +
-            ipt_vel.vyMetersPerSecond * pose.getRotation().getSin();
-        SimpleMatrix x = new SimpleMatrix(new double[][]{{pose.getX()}, {pose.getY()}, {pose.getRotation().getRadians()}});
-        SimpleMatrix x_dot = new SimpleMatrix(new double[][]{{vx0}, {vy0}, {ipt_vel.omegaRadiansPerSecond}});
-        SimpleMatrix acc = root.solve(x, x_dot);
+        SimpleMatrix target = new SimpleMatrix(new double[][]{
+            {speed * pose.getRotation().getCos()},
+            {speed * pose.getRotation().getSin()},
+            {(target_right - target_left) / DifferentialDriveSubsystem.ROBOT_TRACK_WIDTH}});
+        SimpleMatrix x = new SimpleMatrix(
+            new double[][]{{pose.getX()}, {pose.getY()}, {pose.getRotation().getRadians()}});
+        SimpleMatrix acc = target.minus(prev).scale(lambda).plus(root.solve(x, prev));
         long t1 = RobotController.getFPGATime();
-        double dt = (t1-t0) / 1000000;
-        ChassisSpeeds out_vel = new ChassisSpeeds(
-            acc.get(0)*dt + vx0,
-            acc.get(1)*dt + vy0,
-            acc.get(2)*dt + ipt_vel.omegaRadiansPerSecond);
-        DifferentialDriveWheelSpeeds out = kinematics.toWheelSpeeds(out_vel);
-        double out_left = Math.min(1, Math.max(-1, out.leftMetersPerSecond/10));
-        double out_right = Math.min(1, Math.max(-1, out.rightMetersPerSecond/10));
-        drive.setOutput(ControlMode.PercentOutput, out_left, out_right);
+        double dt = (t1 - t0) / 1000000.;
+        SimpleMatrix vel = acc.scale(dt).plus(prev);
+        double fore = vel.get(0) * pose.getRotation().getCos() +
+            vel.get(1) * pose.getRotation().getSin();
+        double left = (fore - DifferentialDriveSubsystem.ROBOT_TRACK_WIDTH*vel.get(2)/2)/10;
+        double right = (fore + DifferentialDriveSubsystem.ROBOT_TRACK_WIDTH*vel.get(2)/2)/10;
+        double max_spd = Math.max(Math.abs(left), Math.abs(right));
+        if(max_spd > 1){
+            right /= max_spd;
+            left /= max_spd;
+            vel.scale(1/max_spd);
+        }
+        drive.setOutput(ControlMode.PercentOutput, left, right);
+        prev = vel;
         t0 = t1;
     }
 }

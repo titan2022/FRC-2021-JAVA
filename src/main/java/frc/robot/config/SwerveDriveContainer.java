377c4711af9235cad2cedff6decb9e6e0cd8b1cb
Jaden <jwang5@imsa.edu>
2021-03-10 19:59:10 -0600

    deleted unused deadband code in container

--- src/main/java/frc/robot/config/SwerveDriveContainer.java
+++ src/main/java/frc/robot/config/SwerveDriveContainer.java
@@ -104,7 +104,6 @@ public class SwerveDriveContainer implements RobotContainer {
         talon.slot0.maxIntegralAccumulator = 254.000000;
         //talon.slot0.closedLoopPeakOutput = 0.869990;
         //talon.slot0.closedLoopPeriod = 33;
-        talon.neutralDeadband = 0.199413;
 
         return talon;
     }

fdeee352c4baded00081ea3a70e324a8468c44fb
cplepel-py <cbplepel@gmail.com>
2021-03-13 15:37:51 -0600

    Added execute and end methods to DriveToGoal command

--- src/main/java/frc/robot/commands/DriveToGoal.java
+++ src/main/java/frc/robot/commands/DriveToGoal.java
@@ -41,4 +41,26 @@ public class DriveToGoal extends CommandBase {
     private SimpleMatrix toVector(Pose2d x) {
         return new SimpleMatrix(new double[][]{{x.getX()}, {x.getY()}, {x.getRotation().getRadians()}});
     }
+    private ChassisSpeeds toIntrinsic(SimpleMatrix v, Rotation2d theta) {
+        return new ChassisSpeeds(v.get(0) * theta.getCos() + v.get(1) * theta.getSin(),
+            v.get(0) * theta.getSin() + v.get(1) * theta.getCos(),
+            v.get(2));
+    }
+
+    @Override
+    public void execute() {
+        double dt = (RobotController.getFPGATime() - t0) / 1000.;
+        SimpleMatrix x1 = toVector(filter.getFilteredPose());
+        SimpleMatrix v0 = x1.minus(x0).scale(dt);
+        SimpleMatrix acc = root.solve(x1, v0);
+        SimpleMatrix v1 = acc.scale(dt).plus(v0);
+        drive.setVelocities(toIntrinsic(v1, new Rotation2d(x1.get(2))));
+        x0 = x1;
+        t0 += dt * 1000;
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        root.unlinkChild(coord);
+    }
 }

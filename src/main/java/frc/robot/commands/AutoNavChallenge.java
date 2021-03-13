96d682a383813f898299c975df990cd1443fe987
cplepel-py <cbplepel@gmail.com>
2021-03-13 16:38:32 -0600

    Added execution methods to AutoNavCommand

--- src/main/java/frc/robot/commands/AutoNavChallenge.java
+++ src/main/java/frc/robot/commands/AutoNavChallenge.java
@@ -2,26 +2,56 @@ package frc.robot.commands;
 
 import org.ejml.simple.SimpleMatrix;
 
+import edu.wpi.first.wpilibj.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.mapping.Point;
 import frc.robot.motion.generation.rmpflow.rmps.Cartesian2d;
 import frc.robot.motion.generation.rmpflow.rmps.GoalAttractor;
 
 public class AutoNavChallenge extends CommandBase {
-    private Point[] waypoints;
+    private Translation2d[] waypoints;
+    private Translation2d waypoint;
     private int next = 0;
     public final RMPDrive drive;
     private Cartesian2d coord;
     private GoalAttractor attractor;
+    private double threshold;
+    private boolean done;
 
-    public AutoNavChallenge(RMPDrive drive, Point... waypoints) {
+    public AutoNavChallenge(RMPDrive drive, double threshold, Translation2d... waypoints) {
         this.drive = drive;
         this.waypoints = waypoints;
+        this.threshold = threshold;
         this.coord = new Cartesian2d("AutoNav 2d", drive.root, 0, 1);
-        this.attractor = new GoalAttractor("Waypoint", this.coord, toVector(new Point(0, 0)), 10, 1, 10, 1, 2, 2, 0.005);
+        waypoint = drive.filter.getFilteredPose().getTranslation();
+        this.attractor = new GoalAttractor("Waypoint", this.coord, toVector(waypoint), 10, 1, 10, 1, 2, 2, 0.005);
     }
 
-    private SimpleMatrix toVector(Point x) {
+    private SimpleMatrix toVector(Translation2d x) {
         return new SimpleMatrix(new double[][]{{x.getX()}, {x.getY()}});
+    }
+
+    @Override
+    public void execute() {
+        if(isFinished())
+            return;
+        if(waypoint.getDistance(drive.filter.getFilteredPose().getTranslation()) < threshold){
+            if(next >= waypoints.length){
+                done = true;
+            }
+            else{
+                waypoint = waypoints[next];
+                attractor.updateGoal(toVector(waypoints[next++]));
+            }
+        }
+    }
+
+    @Override
+    public boolean isFinished() {
+        return done;
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        coord.unlinkParent(coord.getParent());
     }
 }

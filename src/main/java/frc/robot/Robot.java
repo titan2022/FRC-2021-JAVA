0a87240262694af5f8a5ad73d48165dd7935c2dd
ttchalakov <teodor.tch@gmail.com>
2021-03-13 17:30:21 -0600

    Configured galaticsearch container

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -12,6 +12,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.config.DifferentialDriveContainer;
+import frc.robot.config.GalacticSearchContainer;
 import frc.robot.config.RobotContainer;
 
 /**
@@ -28,7 +29,8 @@ public class Robot extends TimedRobot {
   public Robot()
   {
     super(.02); // Default period is .02 seconds = 50 hz
-    robotContainer = new DifferentialDriveContainer(isSimulation());
+    //robotContainer = new DifferentialDriveContainer(isSimulation());
+    robotContainer = new GalacticSearchContainer();
     autoCommandEntryPoint = robotContainer.getAutonomousCommand();
     teleopCommandEntryPoint = robotContainer.getTeleopCommand();
   }
--- src/main/java/frc/robot/config/GalacticSearchContainer.java
+++ src/main/java/frc/robot/config/GalacticSearchContainer.java
@@ -1,22 +1,26 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.config;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import frc.robot.commands.VisionProcessingCommand;
+import frc.robot.subsystems.VisionSubsystem;
+import frc.robot.vision.ExamplePipeline;
 
 /** Add your docs here. */
 public class GalacticSearchContainer implements RobotContainer {
+  private VisionSubsystem visionSub;
 
   private final ParallelCommandGroup autoGroup, teleopGroup;
 
   public GalacticSearchContainer() {
+    visionSub = new VisionSubsystem();
 
     autoGroup = new ParallelCommandGroup(); // These don't actually run in parallel.
     teleopGroup = new ParallelCommandGroup();
 
+    VisionProcessingCommand ballDetector = new VisionProcessingCommand(visionSub, new ExamplePipeline());
+
+    teleopGroup.addCommands(ballDetector);
   }
 
   public Command getAutonomousCommand() {

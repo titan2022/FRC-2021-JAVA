473fb301469cfe3f4793fd8ce32dadde9ef42aa8
rajpatkar <rajan.patkar.email@gmail.com>
2021-03-13 14:06:14 -0600

    Added GalacticSearchContainer

---
+++ src/main/java/frc/robot/config/GalacticSearchContainer.java
@@ -0,0 +1,34 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.config;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+
+/** Add your docs here. */
+public class GalacticSearchContainer implements RobotContainer {
+
+  private final ParallelCommandGroup autoGroup, teleopGroup;
+
+  public GalacticSearchContainer() {
+
+    autoGroup = new ParallelCommandGroup(); // These don't actually run in parallel.
+    teleopGroup = new ParallelCommandGroup();
+
+  }
+
+  public Command getAutonomousCommand() {
+
+    return autoGroup;
+
+  };
+
+  public Command getTeleopCommand() {
+
+    return teleopGroup;
+
+  };
+
+}

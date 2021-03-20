e335f914a9911a98ec2f593f3efdcbcff5191784
ttchalakov <teodor.tch@gmail.com>
2021-03-20 10:46:40 -0500

    Cleaned up Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,10 +1,3 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2019 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
@@ -30,9 +23,6 @@ public class Robot extends TimedRobot {
   {
     super(.02); // Default period is .02 seconds = 50 hz
     // robotContainer = new DifferentialDriveContainer(isSimulation());
-    // autoCommandEntryPoint = robotContainer.getAutonomousCommand();
-    // teleopCommandEntryPoint = robotContainer.getTeleopCommand();
-
     robotContainer = new SwerveDriveContainer(isSimulation());
     autoCommandEntryPoint = robotContainer.getAutonomousCommand();
     teleopCommandEntryPoint = robotContainer.getTeleopCommand();

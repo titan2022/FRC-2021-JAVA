bc0b2b96a243f6e9e2842ee61b1acd8827a5faf4
gchintala <gchintala@imsa.edu>
2021-03-30 22:00:12 -0500

    created sequencer command

--- src/main/java/frc/robot/commands/AutomaticSequencerCommand.java
+++ src/main/java/frc/robot/commands/AutomaticSequencerCommand.java
@@ -3,5 +3,15 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 public class AutomaticSequencerCommand extends CommandBase{
+
+    public void initialize()
+    {
+
+    }
+
+    public void execute(){
+        
+    }
+    
     
 }
